b9717130d54d5a6112f55fb36bc2d0f5d8b5f1bc
Jean Malvessi <jeanmalvessi@gmail.com>
2025-08-03 19:43:02 -0300

    IDs

--- src/test/java/com/algaworks/algashop/ordering/domain/valueobject/CustomerIdTest.java
+++ src/test/java/com/algaworks/algashop/ordering/domain/valueobject/CustomerIdTest.java
@@ -1,6 +1,7 @@
 package com.algaworks.algashop.ordering.domain.valueobject;
 
 import com.algaworks.algashop.ordering.domain.utility.IdGenerator;
+import com.algaworks.algashop.ordering.domain.valueobject.id.CustomerId;
 import org.junit.jupiter.api.Test;
 import java.util.UUID;
 
