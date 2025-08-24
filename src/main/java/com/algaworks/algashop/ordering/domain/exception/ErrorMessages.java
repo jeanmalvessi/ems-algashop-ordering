f0f8902d0b3c431c2d531be1096006df2961fd68
Jean Malvessi <jeanmalvessi@gmail.com>
2025-08-24 18:26:46 -0300

    ShoppingCart aggregate

--- src/main/java/com/algaworks/algashop/ordering/domain/exception/ErrorMessages.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/exception/ErrorMessages.java
@@ -31,4 +31,8 @@ public class ErrorMessages {
 
     public static final String ERROR_PRODUCT_IS_OUT_OF_STOCK = "Product %s is out of stock";
 
+    public static final String ERROR_SHOPPING_CART_DOES_NOT_CONTAIN_ITEM = "Shopping Cart %s does not contain item %s";
+    public static final String ERROR_SHOPPING_CART_DOES_NOT_CONTAIN_PRODUCT = "Shopping Cart %s does not contain product %s";
+    public static final String ERROR_SHOPPING_CART_ITEM_INCOMPATIBLE_PRODUCT = "Shopping Cart %s cannot be updated, incompatible product %s";
+
 }
