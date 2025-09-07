19421f56a7bdcbca09d604ea289aea76021bd661
Jean Malvessi <jeanmalvessi@gmail.com>
2025-09-07 18:13:21 -0300

    Order auditing values

--- src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/entity/OrderPersistenceEntity.java
+++ src/main/java/com/algaworks/algashop/ordering/infrastructure/persistence/entity/OrderPersistenceEntity.java
@@ -1,9 +1,14 @@
 package com.algaworks.algashop.ordering.infrastructure.persistence.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
@@ -14,9 +19,10 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @ToString(of = "id")
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-@Builder
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "\"order\"")
 public class OrderPersistenceEntity {
 
@@ -35,5 +41,14 @@ public class OrderPersistenceEntity {
     private OffsetDateTime paidAt;
     private OffsetDateTime canceledAt;
     private OffsetDateTime readyAt;
+
+    @CreatedBy
+    private UUID createdByUserId;
+
+    @LastModifiedDate
+    private OffsetDateTime lastModifiedAt;
+
+    @LastModifiedBy
+    private UUID lastModifiedByUserId;
 
 }
