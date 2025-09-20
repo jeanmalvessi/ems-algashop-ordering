9599910371f815540fb5bdaadcb715792e7afc30
Jean Malvessi <jeanmalvessi@gmail.com>
2025-09-20 09:29:55 -0300

    Order items persistence

--- src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
+++ src/test/java/com/algaworks/algashop/ordering/infrastructure/persistence/repository/OrderPersistenceEntityRepositoryIT.java
@@ -24,6 +24,9 @@ class OrderPersistenceEntityRepositoryIT {
 
         orderPersistenceEntityRepository.saveAndFlush(entity);
         Assertions.assertThat(orderPersistenceEntityRepository.existsById(entity.getId())).isTrue();
+
+        OrderPersistenceEntity savedEntity = orderPersistenceEntityRepository.findById(entity.getId()).orElseThrow();
+        Assertions.assertThat(savedEntity.getItems()).isNotEmpty();
     }
 
     @Test
