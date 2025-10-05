c2ff6fb4669ccc4a4a1e9b96bd4a8ca2079ed2f7
Jean Malvessi <jeanmalvessi@gmail.com>
2025-10-05 18:29:52 -0300

    Customer archive

--- src/main/java/com/algaworks/algashop/ordering/application/customer/management/CustomerManagementApplicationService.java
+++ src/main/java/com/algaworks/algashop/ordering/application/customer/management/CustomerManagementApplicationService.java
@@ -89,4 +89,14 @@ public class CustomerManagementApplicationService {
 
         customers.add(customer);
     }
+
+    @Transactional
+    public void archive(UUID rawCustomerId) {
+        Customer customer = customers.ofId(new CustomerId(rawCustomerId))
+                .orElseThrow(CustomerNotFoundException::new);
+
+        customer.archive();
+
+        customers.add(customer);
+    }
 }
