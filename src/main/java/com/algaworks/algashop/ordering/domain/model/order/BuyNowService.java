d4a5e07f934b11b35cb10e4ffae79608e7ad5f04
Jean Malvessi <jeanmalvessi@gmail.com>
2025-10-12 15:28:26 -0300

    Free shipping rules

--- src/main/java/com/algaworks/algashop/ordering/domain/model/order/BuyNowService.java
+++ src/main/java/com/algaworks/algashop/ordering/domain/model/order/BuyNowService.java
@@ -1,25 +1,47 @@
 package com.algaworks.algashop.ordering.domain.model.order;
 
 import com.algaworks.algashop.ordering.domain.model.DomainService;
+import com.algaworks.algashop.ordering.domain.model.commons.Money;
+import com.algaworks.algashop.ordering.domain.model.customer.Customer;
+import com.algaworks.algashop.ordering.domain.model.customer.LoyaltyPoints;
 import com.algaworks.algashop.ordering.domain.model.product.Product;
 import com.algaworks.algashop.ordering.domain.model.commons.Quantity;
 import com.algaworks.algashop.ordering.domain.model.customer.CustomerId;
+import lombok.RequiredArgsConstructor;
+
+import java.time.Year;
 
 @DomainService
+@RequiredArgsConstructor
 public class BuyNowService {
 
-    public Order buyNow(Product product, CustomerId customerId, Billing billing,
+    private final Orders orders;
+
+    public Order buyNow(Product product, Customer customer, Billing billing,
                         Shipping shipping, Quantity quantity, PaymentMethod paymentMethod) {
 
         product.checkOutOfStock();
 
-        Order order = Order.draft(customerId);
+        Order order = Order.draft(customer.id());
         order.changeBilling(billing);
-        order.changeShipping(shipping);
         order.changePaymentMethod(paymentMethod);
         order.addItem(product, quantity);
+
+        if (hasFreeShipping(customer)) {
+            Shipping freeShipping = shipping.toBuilder().cost(Money.ZERO).build();
+            order.changeShipping(freeShipping);
+        } else {
+            order.changeShipping(shipping);
+        }
+
         order.place();
 
         return order;
+    }
+
+    private boolean hasFreeShipping(Customer customer) {
+        return customer.loyaltyPoints().compareTo(new LoyaltyPoints(100)) >= 0
+                && orders.salesQuantityByCustomerInYear(customer.id(), Year.now()) >= 2
+                || customer.loyaltyPoints().compareTo(new LoyaltyPoints(2000)) >= 0;
     }
 }
