801c5db912b9189aee2050b6ce769bc824591d0e
Jean Malvessi <jeanmalvessi@gmail.com>
2025-11-30 20:41:28 -0300

    RestAssured integration tests

--- src/test/java/com/algaworks/algashop/ordering/contract/base/OrderBase.java
+++ src/test/java/com/algaworks/algashop/ordering/contract/base/OrderBase.java
@@ -6,7 +6,7 @@ import com.algaworks.algashop.ordering.application.checkout.CheckoutApplicationS
 import com.algaworks.algashop.ordering.application.checkout.CheckoutInput;
 import com.algaworks.algashop.ordering.application.order.query.*;
 import com.algaworks.algashop.ordering.domain.model.order.OrderNotFoundException;
-import com.algaworks.algashop.ordering.presentation.OrderController;
+import com.algaworks.algashop.ordering.presentation.order.OrderController;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
 import org.mockito.Mockito;
