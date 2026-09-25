9c8f40e52ec5f16b3f11978c50a8d0e6373109f8
Jean Malvessi <frwk.jean@unimedpoa.com.br>
2026-09-25 14:31:33 -0300

    Customized TypeId in deserialization

--- src/main/java/com/algaworks/algashop/ordering/core/application/product/event/ProductListedIntegrationEvent.java
+++ src/main/java/com/algaworks/algashop/ordering/core/application/product/event/ProductListedIntegrationEvent.java
@@ -1,15 +1,21 @@
-package com.algaworks.algashop.ordering.infrastructure.adapters.input.messaging.kafka.product;
+package com.algaworks.algashop.ordering.core.application.product.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.time.OffsetDateTime;
 import java.util.UUID;
 
 @Getter
+@Setter
 @ToString
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductListedIntegrationEvent {
     private UUID productId;
     private OffsetDateTime listedAt;
